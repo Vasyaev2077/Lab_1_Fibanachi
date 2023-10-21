c104a57538c48c15cec0401beede8b64c06e8c52
Vasyaev2077 <vasyaev9999@gmail.com>
2023-10-21 13:34:45 +0500

    Fibanachi check

---
+++ src/Fib_lab1/Fib1.java
@@ -0,0 +1,12 @@
+package Fib_lab1;
+
+public class Fib1 {
+    private static int fib1(int n) {
+        return fib1(n - 1) + fib1(n - 2);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(fib1(5));
+    }
+
+}
